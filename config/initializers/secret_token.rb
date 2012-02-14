21db9620bc91d5e998243f4fe349fd208017785a
sherpc <aleksandrsher@gmail.com>
2012-02-14 21:57:10 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookCodes::Application.config.secret_token = 'efc6c33bb10a100ad905aa3889bf80bb50ef92428edbb018e1572f9d82e67bbfa25173c090a3990ed784e99a3da29c5a2f682c322ae6fbeb9a9abb65e096b64c'
